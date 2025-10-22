c84e595625e4bf0a7e2a449c0b44d7cfa0e1dd59
Alex Stoev <alex.stoev@gmail.com>
2025-10-22 12:50:13 +0300

    Code clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { LexDown } from "./lexdown/LexDown";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-red-600">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="min-h-screen min-w-full bg-slate-800 p-6 text-slate-200">
+      <h1 className="border-b border-gray-600">Lex down</h1>
+      <LexDown />
+    </div>
   );
 }
 
